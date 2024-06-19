1a08046ab48f62236dedbf69cadaba20bec76c05
FabienTreve <trevefabien@gmail.com>
2024-06-19 09:04:30 +0200

    Creating models and admin page

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name='movies')
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    grade = models.IntegerField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return f"Review(s) pour {self.movie.title}"
